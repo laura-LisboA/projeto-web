425d7bb89e51dc47c04b494da213fb04ba394296
JunimCria <gilbertojuniorcc@gmail.com>
2023-05-23 15:08:35 -0300

    Atualizando estilo

--- index.php
+++ index.php
@@ -13,14 +13,27 @@
     <div class='container'>
         <h1 class="nome-loja">Fashion Reborn</h1>
         <div class="menu-principal">
-            <button class='menu-button'>Moda Feminina</button>
-            <button class='menu-button'>Moda Masculina</button>
-            <button class='menu-button'>Moda Infantil</button>
+            <div>
+                <div class="select-menu moda-feminina">
+                </div>
+                <h2 class="title-menu texto">Moda Feminina</h2>
+            </div>
+            <div>
+                <div class="select-menu moda-masculina">
+                </div>
+                <h2 class="title-menu texto">Moda Masculina</h2>
+            </div>
+            <div>
+                <div class="select-menu moda-kids">
+                </div>
+                <h2 class="title-menu texto">Moda Kids</h2>
+            </div>
         </div>
     </div>
     <footer class='footer'>
         <p style="font-family:sans-serif;">©Todos Direitos Reservados FashionReborn Ltda.</p>
     </footer>
 </body>
+
 
 </html>
