8bf773b8cd8f66950ca849f4e523825268ee1256
laura-LisboA <laurasantoa40@gmail.com>
2023-05-23 10:28:58 -0300

    arquivo php

---
+++ roupasfemininas.php
@@ -0,0 +1,12 @@
+<?php
+
+$blusas = [
+    [        
+        "tipo" => "Blusa",
+        "marca" => "sem-marca",
+        "tamanho" => "P",
+        "foto" => "imagens/1.jpg",
+        "preco" => 20,        
+        "descricao" => "linda blusa branca 100% algodão, elegante.",
+        "condicao" => "usada"
+    ],
