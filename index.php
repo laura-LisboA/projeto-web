764a7420ce3cd2dbced7faddf3929987d87bc440
laura-LisboA <laurasantoa40@gmail.com>
2023-05-24 21:03:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Fashion Reborn</title>
 </head>
 
 <body>
@@ -27,9 +27,9 @@
                 <nav>
 				    <ul>
 					   <li><a href="">Home</a></li>
-					   <li><a href="">Serviços</a></li>
+					   <li><a href="">Roupas</a></li>
 					   <li><a href="">Informações</a></li>
-					   <li><a href="">Contato</a></li>
+					   <li><a href="">Entrar</a></li>
 				    </ul>	
                 </nav>
             </div>
