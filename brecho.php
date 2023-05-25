9129c4501770fcf32cc7e56a951f8f38e79a498e
laura-LisboA <laurasantoa40@gmail.com>
2023-05-24 21:02:57 -0300

    Create brecho.php

---
+++ brecho.php
@@ -0,0 +1,33 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Roupas</title>
+	<link rel="stylesheet" type="text/css" href="funcionarios.css">
+</head>
+<body>
+    <header>
+    <div class="botao">
+		<a href="formulario.php">Área do cliente</a>
+	</div>
+    <div id="menu">
+                <div>
+                    <img src="images/logo.png">
+                </div>
+                <nav>
+				    <ul>
+					   <li><a href="">Home</a></li>
+					   <li><a href="">Roupas</a></li>
+					   <li><a href="">Informações</a></li>
+					   <li><a href="">Entrar</a></li>
+				    </ul>	
+                </nav>
+            </div>
+    </header>
+	<div class="container-detalhe">
+		
+	</div>
+</body>
+</html>
